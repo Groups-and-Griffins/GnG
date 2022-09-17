b699e376b230e46095084739c29955e6f693198d
Michael <mhessler735@gmail.com>
2022-09-17 00:03:03 -0700

    login/signup implementation

---
+++ src/config/fire.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+var firebaseConfig = {
+    apiKey: "AIzaSyAFo1h1JapdUC3zT_m_fns69UmD41wI6Q4",
+    authDomain: "groups-n-griffins.firebaseapp.com",
+    projectId: "groups-n-griffins",
+    storageBucket: "groups-n-griffins.appspot.com",
+    messagingSenderId: "1019692112643",
+    appId: "1:1019692112643:web:96bdd115faa9eb28036736",
+    measurementId: "G-7K3K68RFFX"
+  };
+
+const fire = firebase.initializeApp(firebaseConfig);
+export const auth = firebase.auth();
+export default fire;
