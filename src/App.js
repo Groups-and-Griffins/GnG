1e6883c8c09241e154d106d069ebbc592a305d97
Michael <mhessler@Jarvis.localdomain>
2022-09-24 20:14:52 -0700

    Updated styling of login signup

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React, {Component} from 'react';
-import Home from './Home.js';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
+import Home from './Dashboard.js';
 import Login from './Login.js';
+import SignUp from './Signup.js';
+import { AuthProvider } from "./AuthContext";
 
 import fire from './config/fire';
 
@@ -32,10 +36,46 @@ class App extends Component{
 
   render() {
     return (
-      <div className="App">
-      { this.state.user ? ( <Home/> ) : ( <Login/> ) }
-      </div>
-    );
+      <Router>
+        <div className="App">
+          <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+            <div className="container">
+              <Link className="navbar-brand" to={'/sign-in'}>
+                Groups And Griffins
+              </Link>
+              <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+                <ul className="navbar-nav ml-auto">
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-in'}>
+                      Login
+                    </Link>
+                  </li>
+                  <li className="nav-item">
+                    <Link className="nav-link" to={'/sign-up'}>
+                      Sign up
+                    </Link>
+                  </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+          <div className="auth-wrapper">
+            <div className="auth-inner">
+              <Routes>
+                <Route exact path="/" element={<Login />} />
+                <Route path="/sign-in" element={<Login />} />
+                <Route path="/sign-up" element={<SignUp />} />
+                <Route path="/home" element={<Home />} />
+              </Routes>
+            </div>
+          </div>
+        </div>
+      </Router>
+    )
+      // <div className="App">
+      // { this.state.user ? ( <Home/> ) : ( <Login/> ) }
+      // </div>
+    // );
   }
 }
 
--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import fire from './config/fire';
+import Dashboard from './Dashboard.js';
+
 
 class Login extends React.Component {
-
-    login () {
+    login() {
         const email = document.querySelector('#email').value;
         const password = document.querySelector('#password').value;
 
@@ -31,19 +32,52 @@ class Login extends React.Component {
 
     render() {
         return (
-            <div style={{ textAlign: 'center' }}>
-              <div>
-                <div>Email</div>
-                <input id="email" placeholder="Enter Email.." type="text"/>
-              </div>
-              <div>
-                <div>Password</div>
-                <input id="password" placeholder="Enter Password.." type="text"/>
-              </div>
-              <button style={{margin: '10px'}} onClick={this.login}>Login</button>
-              <button style={{margin: '10px'}} onClick={this.signUp}>Sign Up</button>
-            </div>
-          )
+            <form>
+                <h3>Sign In</h3>
+                <div className="mb-3">
+                    <label>Email address</label>
+                    <input
+                        type="email"
+                        className="form-control"
+                        placeholder="Enter email"
+                    />
+                </div>
+                <div className="mb-3">
+                    <label>Password</label>
+                    <input
+                        type="password"
+                        className="form-control"
+                        placeholder="Enter password"
+                    />
+                </div>
+                <div className="mb-3">
+                    <div className="custom-control custom-checkbox d-inline-block">
+                        <input type="checkbox" className="custom-control-input" id="customCheck1" />
+                        <label className="custom-control-label" htmlFor="customCheck1"> Remember me </label>
+                    </div>
+                </div>
+                <div className="d-grid">
+                    <button type="submit" className="btn btn-primary" onClick={this.login}>Submit</button>
+                </div>
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+            </form>
+
+            // <div style={{ textAlign: 'center',  backgroundImage: `url("https://via.placeholder.com/500")` }}>
+            //   <div>
+            //     <h1>Groups N Griffins</h1>
+            //     <div>Email</div>
+            //     <input id="email" placeholder="Enter Email.." type="text"/>
+            //   </div>
+            //   <div>
+            //     <div>Password</div>
+            //     <input id="password" placeholder="Enter Password.." type="text"/>
+            //   </div>
+            //   <button style={{margin: '10px'}} onClick={this.login}>Login</button>
+            //   <button style={{margin: '10px'}} onClick={this.signUp}>Sign Up</button>
+            // </div>
+        )
     }
 }
 
