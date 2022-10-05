164df1432d2a29b25918b8b8970384907040dd11
Samsational-9001 <sammchilders@gmail.com>
2022-10-04 20:32:05 -0700

    For A4 made code for part 2

---
+++ WebControllerSam.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { grommet, Box, Heading, Grommet, Paragraph } from "grommet";
+
+const SamApp = () => (
+  <Grommet theme={grommet}>
+    <Box pad="small">
+      <Heading>Hello Griffins</Heading>
+    </Box>
+  </Grommet>
+);
+
+export default SamApp;
