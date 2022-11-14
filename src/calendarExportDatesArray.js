db7e309c41a32ada9e20823bc6aa277e402240b6
Michael Hessler <85195875+mhessler735@users.noreply.github.com>
2022-11-13 21:49:16 -0800

    Removed unecessary code

--- src/calendarExportDatesArray.js
+++
@@ -1,3 +0,0 @@
-//export sends an array of the dates
-import fire from './config/fire';
-import {db} from './config/fire';
--- src/calendarImportDatesArray.js
+++
@@ -1,7 +0,0 @@
-//import puts into database
-import fire from './config/fire';
-import {db} from './config/fire';
-
-insertDatesIntoDatabase() {
-    let 
-}
