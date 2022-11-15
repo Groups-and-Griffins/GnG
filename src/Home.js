4c1b790d6d61e32802271da409a1c2d115a650cf
Samsational-9001 <sammchilders@gmail.com>
2022-11-14 20:50:12 -0800

    made logo

--- src/Home.js
+++ src/Home.js
@@ -163,7 +163,7 @@ export default function Home() {
       <div className="container">
         <div className="custom_heading-container">
           <h2>
-            Welcome To Our Company
+            Welcome To Our Griffin Crew
           </h2>
         </div>
         <div className="layout_padding2">
@@ -172,11 +172,7 @@ export default function Home() {
           </div>
           <div className="detail-box">
             <p>
-              dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna
-              aliqua.
-              Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
-              Duis
-              aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu
+              We, the griffins, are working hard so that this website can help you start scheduling a consistant DnD group. 
             </p>
             {/* <div>
               <a href>
