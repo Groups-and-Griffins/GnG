11862f5d16601138d39bda5baca7a3a74d73084c
Samsational-9001 <sammchilders@gmail.com>
2022-11-21 21:26:02 -0800

    Ok we want the team page to be different if the person is a DM and they already have a team

--- src/CreateTeam.js
+++ src/CreateTeam.js
@@ -1,6 +1,27 @@
 import React from 'react'
 
+<<<<<<< Updated upstream
 export default function CreateTeam() {
+=======
+
+    if(querySnapshot.empty) {
+      const data = {
+        team: teamName,
+        DMEmail: playerEmail,
+        description: des
+      };
+      setDoc(docRef, data)
+      .then(() => {
+        console.log("Document has been added successfully");
+      })
+
+      navigate("/team");
+    }
+    else {
+      return setError("Team Name already exists");
+    }
+  }
+>>>>>>> Stashed changes
   return (
     <div>
       Create a team
--- src/Team.js
+++ src/Team.js
@@ -9,8 +9,15 @@ import {collection, updateDoc, setDoc, doc, getDoc, getDocs, onSnapshot, deleteD
 export default function Team() {
   let navigate = useNavigate();
   const [playerRole, setCurrentPlayerRole] = useState("");
+<<<<<<< Updated upstream
   const [showElement, setShowElement] = useState(false)
 
+=======
+  const [playerEmail, setCurrentEmail] = useState("");
+  const [showElement, setShowElement] = useState(false);
+  const [teamName, setCurrentTeamName] = useState("");
+  const [teamDesc, setCurrentTeamDesc] = useState("");
+>>>>>>> Stashed changes
   useEffect(() => {
     const fetchData = async() => {
       try {
@@ -27,16 +34,68 @@ export default function Team() {
       setShowElement(true);
     } 
     fetchData();
+<<<<<<< Updated upstream
   }, []);
   
   console.log(playerRole);
   
+=======
+ 
+  const fetchData2 = async() => {
+    try {
+      const usersRef = collection(db, "teams");
+      const q = query(usersRef, where("DMEmail", "==", playerEmail));
+      const querySnapshot2 = await getDocs(q);
+      querySnapshot2.forEach((doc) => {
+        console.log(doc.data().DMEmail);
+      })
+      if(querySnapshot2.exists()){
+        setCurrentTeamName(querySnapshot2.data().team);
+        setCurrentTeamDesc(querySnapshot2.data().description);
+        document.getElementById("teamName").innerHTML = "THIS WORKS";
+        document.getElementById("teamDescript").innerHTML = teamDesc;
+        document.write(teamName+teamDesc);
+      }
+    } catch(err) {
+      console.error(err);
+    }
+  }
+  fetchData2();
+
+  const fetchData3 = async() => {
+    try {
+      const docRef = doc(db, "teams", fire.auth().currentUser.uid);
+      const docSnap = await getDoc(docRef);
+      if (docSnap.exists()) {
+        setCurrentPlayerRole(docSnap.data().playerRole);
+        setCurrentEmail(docSnap.data().email);
+      } else {
+        console.error("can't find user");
+      }
+    } catch(err) {
+      console.error(err);
+    }
+    setShowElement(true);
+  } 
+  fetchData3();
+}, []);
+   //console.log(teamName);
+
+
+
+>>>>>>> Stashed changes
   if (playerRole == "Player") {
     document.querySelector('#teamButton').innerText = 'Join a team';
   }
+  //If the user is a DM and if their Email is already connected to a team they should see that teams info
   else if (playerRole == "DM") {
-    document.querySelector('#teamButton').innerText = 'Create a team';
+    if(teamName != ""){
+      document.querySelector('#teamButton').innerText = 'Create a team';
+    }else if(teamName == ""){
+      document.querySelector('#teamButton').innerText = 'Test';
+    }
   }
+
 
   async function handleSubmit(e) {
     if (playerRole == "Player")
@@ -45,6 +104,7 @@ export default function Team() {
       navigate('/createTeam');
 }
   
+<<<<<<< Updated upstream
   return (
     <>
         <SideNavBar />
@@ -60,4 +120,45 @@ export default function Team() {
       </Container>
     </>
   )
+=======
+  if(playerRole == "DM" && teamName != ""){
+    //no button if the DM already has a team created
+    return (
+      <>
+          <SideNavBar />
+          <header className="custom_navbar">
+          <span id="myDiv" style={{ color: "#FFF", fontSize: "25px", paddingLeft: "5rem" }}>
+            My Team Exists
+          </span>
+        </header>
+        <Container className="d-flex align-items-center justify-content-center" >
+        <body>
+        Hello
+        </body>
+        </Container>
+        <div>
+        <h1> <span id="teamName">Team Name</span> </h1>
+          <p> <span id="teamDescript">Team Descript</span> </p>
+        </div>
+        
+      </>
+    )
+  }else{
+    return (
+      <>
+          <SideNavBar />
+          <header className="custom_navbar">
+          <span id="myDiv" style={{ color: "#FFF", fontSize: "25px", paddingLeft: "5rem" }}>
+            My Team Create
+          </span>
+        </header>
+        <Container className="d-flex align-items-center justify-content-center" >
+          {showElement ? (<Button id = "teamButton" onClick = {handleSubmit} style= {{marginTop: "20rem"}}>
+          </Button> ): (<Button id = "teamButton" onClick = {handleSubmit} style= {{marginTop: "20rem", display: "none"}}>
+          </Button> )}
+        </Container>
+      </>
+    )
+  }
+>>>>>>> Stashed changes
 }
